760e5cd2c511e961ec7765875c3cf9e53367a736
fmfahath <fmfahath.dev@gmail.com>
2024-04-16 09:19:39 +0530

    product model : migration (table) created

--- database/migrations/2024_04_16_034019_create_products_table.php
+++ database/migrations/2024_04_16_034019_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('mrp');
+            $table->string('price');
+            $table->string('image');
             $table->timestamps();
         });
     }
